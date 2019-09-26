001de9477a815498dba50c17c2a065b4aa6f94cd
alexfilipe <alex@afcbs.me>
2019-09-26 01:22:17 -0400

    finished responsive wireframe

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-// Fixed sidebar on scroll for smaller
+// Fixed sidebar on scroll for smaller viewports
 window.onload = function() {
     var sidebar = document.getElementById("main-header");
     var y = window.scrollY;
     var stop = 65;
 
-    if (window.innerWidth <= 736) stop = 45;
-    if (y > stop) sidebar.className = "fixed";
+    if(window.innerWidth > 534) {
+        if (window.innerWidth <= 736) stop = 45;
+        if (y > stop) sidebar.className = "fixed";
+    }
 
     window.onscroll = function() {
-        if (window.innerWidth <= 736) stop = 45;
-        else stop = 65;
+        if(window.innerWidth > 534) {
+            if (window.innerWidth <= 736) stop = 45;
+            else stop = 65;
 
-        y = window.scrollY;
+            y = window.scrollY;
 
-        if (y > stop) sidebar.className = "fixed";
-        else sidebar.className = "";
+            if (y > stop) sidebar.className = "fixed";
+            else sidebar.className = "";
+        }
     }
 }
